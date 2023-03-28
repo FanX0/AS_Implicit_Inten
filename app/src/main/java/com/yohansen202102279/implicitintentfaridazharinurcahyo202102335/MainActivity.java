9865ed5df61a03ab7a4b9bb179ad526e71769f2d
farid azhari <faridazhari46@gmail.com>
2023-03-28 09:43:48 +0700

    Initial commit

---
+++ app/src/main/java/com/yohansen202102279/implicitintentfaridazharinurcahyo202102335/MainActivity.java
@@ -0,0 +1,14 @@
+package com.yohansen202102279.implicitintentfaridazharinurcahyo202102335;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
