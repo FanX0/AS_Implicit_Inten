ad54fee6903168bfb1e18cb38208c543d2d8a5dd
jihadaziezi <127930364+jihadaziezi@users.noreply.github.com>
2023-03-30 10:02:24 +0700

    add google drive

--- app/src/main/java/com/yohansen202102279/implicitintentfaridazharinurcahyo202102335/MainActivity.java
+++ app/src/main/java/com/yohansen202102279/implicitintentfaridazharinurcahyo202102335/MainActivity.java
@@ -9,6 +9,7 @@ import android.os.Bundle;
 import android.provider.Settings;
 import android.view.View;
 import android.widget.Toast;
+import android.net.Uri;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,72 +20,89 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void tampilTelepon(View view) {
-        Intent teleponIntent =  new Intent(Intent.ACTION_DIAL);
+        Intent teleponIntent = new Intent(Intent.ACTION_DIAL);
         startActivity(teleponIntent);
     }
+
     public void tampilSms(View view) {
         Intent smsIntent = new Intent(Intent.ACTION_MAIN);
         smsIntent.addCategory(Intent.CATEGORY_APP_MESSAGING);
         startActivity(smsIntent);
     }
+
     public void tampilanKalender(View view) {
         Intent kalenderIntent = new Intent(Intent.ACTION_MAIN);
         kalenderIntent.addCategory(Intent.CATEGORY_APP_CALENDAR);
         startActivity(kalenderIntent);
     }
-    public void  tampilBrowser(View view) {
+
+    public void tampilBrowser(View view) {
         Intent browserIntent = new Intent(Intent.ACTION_MAIN);
         browserIntent.addCategory(Intent.CATEGORY_APP_BROWSER);
         startActivity(browserIntent);
     }
 
-    public void tampilKalkulator(View view)
-    {
-        try
-    {
-        Intent kalkulatorIntent = new Intent(Intent.ACTION_MAIN);
-        kalkulatorIntent.addCategory(Intent.CATEGORY_LAUNCHER);
+    public void tampilKalkulator(View view) {
+        try {
+            Intent kalkulatorIntent = new Intent(Intent.ACTION_MAIN);
+            kalkulatorIntent.addCategory(Intent.CATEGORY_LAUNCHER);
 
-        ComponentName cn = new ComponentName("com.android.calculator2","com.android.calculator2.Calculator");
-        kalkulatorIntent.setComponent(cn);
-        startActivity(kalkulatorIntent);
-    }
-        catch (ActivityNotFoundException anfe)
-    {
-        Toast.makeText(getApplicationContext(), "Aplikasi tidak ditemukan", Toast.LENGTH_LONG).show();
-    }
+            ComponentName cn = new ComponentName("com.android.calculator2", "com.android.calculator2.Calculator");
+            kalkulatorIntent.setComponent(cn);
+            startActivity(kalkulatorIntent);
+        } catch (ActivityNotFoundException anfe) {
+            Toast.makeText(getApplicationContext(), "Aplikasi tidak ditemukan", Toast.LENGTH_LONG).show();
+        }
     }
 
     public void tampilKontak(View view) {
-        Intent kontakIntent = new Intent (Intent.ACTION_MAIN);
+        Intent kontakIntent = new Intent(Intent.ACTION_MAIN);
         kontakIntent.addCategory(Intent.CATEGORY_APP_CONTACTS);
         startActivity(kontakIntent);
     }
 
-    public  void tampilGaleri(View vew) {
+    public void tampilGaleri(View view) {
         Intent galeriIntent = new Intent(Intent.ACTION_MAIN);
         galeriIntent.addCategory(Intent.CATEGORY_APP_GALLERY);
         startActivity(galeriIntent);
     }
+
     public void tampilWifi(View view) {
-        Intent wifiIntent =  new Intent(Settings.ACTION_WIFI_SETTINGS);
+        Intent wifiIntent = new Intent(Settings.ACTION_WIFI_SETTINGS);
         startActivity(wifiIntent);
     }
+
     public void tampilSound(View view) {
         Intent soundIntent = new Intent(Settings.ACTION_SOUND_SETTINGS);
         startActivity(soundIntent);
     }
+
     public void tampilAirplane(View view) {
         Intent airplaneIntent = new Intent(Settings.ACTION_AIRPLANE_MODE_SETTINGS);
         startActivity(airplaneIntent);
     }
-    public void tampilAplikasi (View view) {
+
+    public void tampilAplikasi(View view) {
         Intent aplikasiIntent = new Intent(Settings.ACTION_APPLICATION_SETTINGS);
         startActivity(aplikasiIntent);
     }
+
     public void tampilBluetooth(View view) {
         Intent bluetoothIntent = new Intent(Settings.ACTION_BLUETOOTH_SETTINGS);
         startActivity(bluetoothIntent);
     }
 
+    public void tampilGoogleDrive(View view) {
+        try {
+            Intent googleDriveIntent = new Intent(Intent.ACTION_MAIN);
+            googleDriveIntent.addCategory(Intent.CATEGORY_LAUNCHER);
+
+            ComponentName cn = new ComponentName("com.google.android.apps.docs", "com.google.android.apps.docs.app.NewMainProxyActivity");
+            googleDriveIntent.setComponent(cn);
+
+            startActivity(googleDriveIntent);
+        } catch (ActivityNotFoundException e) {
+            Toast.makeText(getApplicationContext(), "Aplikasi tidak ditemukan", Toast.LENGTH_LONG).show();
+        }
+    }
 }
